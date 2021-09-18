c86e0125a14e57de67d040f3fbd08f714e7590b0
kenoaljiji <studioaljiji@gmail.com>
2021-09-18 10:25:23 +0200

    find a character

---
+++ ex13/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+  // Only change code below this line
+
+  var lastName = "Bezos";
+  var thirdTolastLetterOfLastName = lastName[lastName.length - 3];
+
+  // Only change code above this line
+
+  return thirdTolastLetterOfLastName;
+}
+console.log(main());
+
+module.exports = main;
